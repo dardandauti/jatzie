a33a22660e9de9ee288baf006ab3ae46c8de4785
dardan <dardandauti@live.se>
2025-11-24 19:11:56 +0100

    removed more mantine

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import "@mantine/core/styles.css";
 import App from "./App.tsx";
-import { MantineProvider } from "@mantine/core";
 
 createRoot(document.getElementById("root")!).render(
-  <MantineProvider>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </MantineProvider>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
